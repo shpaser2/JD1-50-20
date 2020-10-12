a1459a7d79127aa0c7e1c9fc6a326720ca0aff67
Sergey Shpakovsky <shpaser3@yandex.ru>
2020-10-13 02:17:54 +0300

    feat: classes Person and Animal with comparators were created

---
+++ homework/src/homework6/Animal.java
@@ -0,0 +1,19 @@
+package homework6;
+
+public class Animal {
+    private int age;
+    private String nick;
+
+    public Animal(int age, String nick) {
+        this.age = age;
+        this.nick = nick;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+}
