3a059532c3015536ec10f118f549c7f7734d381a
Sergei Shpakovski <shpaser2@yandex.ru>
2020-11-01 09:38:33 +0300

    feat: menu to select bank added

--- homework/src/homework8/SiteLoader/Test.java
+++ homework/src/homework8/SiteLoader/Test.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-//        System.out.println("продажа, НБРБ");
-//        printRates(new NBRBLoader());
-//        System.out.println("продажа, БПС");
-//        printRates(new BPSLoader());
-        loadRatesBAB();
+        switch (chooseBank()) {
+            case 1:
+                System.out.println("продажа, НБРБ");
+                printRates(new NBRBLoader());
+                break;
+            case 2:
+                loadRatesBAB();
+                break;
+            case 3:
+                System.out.println("продажа, БПС");
+                printRates(new BPSLoader());
+                break;
+            default:
+                break;
+        }
     }
 
     private static void printRates(SiteLoader loader){
@@ -39,7 +49,7 @@ public class Test {
             stringWithDate = console.nextLine();
             date = TransformDate.transformDate(stringWithDate);
         } while(date == null);
-        System.out.println("продажа, БелАгроБанк");
+        System.out.println("продажа, БелАгропромБанк");
         if (TransformDate.isWeekend(date, "MM/dd/yyyy")) {
             System.out.println("Это выходной день. Актуальные курсы не доступны");
         } else {
@@ -47,6 +57,33 @@ public class Test {
         }
     }
 
+    public static int chooseBank() {
+        int bankNumber = 0;
+        String buffer;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            printBanksList();
+            try {
+                buffer = scanner.nextLine();
+                bankNumber = Integer.parseInt(buffer);
+            } catch (NumberFormatException e) {
+                System.out.println("Введите число от 1 до 3");
+            }
+        } while (bankNumber != 1 && bankNumber != 2 && bankNumber != 3);
+        return bankNumber;
+    }
+
+    private static void printBanksList() {
+        System.out.println("Выберите порядковое число банка для " +
+                "запроса курса валют.");
+
+        System.out.println("1. Национальный Банк - курс по дате или " +
+                "диапазону дат. Для любых дней недели и года.");
+        System.out.println("2. БелАгропромБанк - курс по дате или " +
+                "диапазону дат. Только для рабочих дней.");
+        System.out.println("3. БПС-Банк - только текущий курс, актуальный" +
+                "на сегодня.");
+    }
 
 //    private static void loadRatesInRange() {
 //        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
